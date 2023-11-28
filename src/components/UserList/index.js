df421160906ea7b9ccb7db3feae182d7c1c75202
Gergana <onyxblack2016@gmail.com>
2023-11-28 08:17:21 +0200

    UserList key=_id

--- src/components/UserList/index.js
+++ src/components/UserList/index.js
@@ -15,7 +15,7 @@ const UserList = () => {
          <h2>User List</h2>
          <ul>
             {users.map(user => (
-               <li key={user.id}>{user.username}</li>
+               <li key={user._id}>{user.username}</li>
             ))}
          </ul>
       </div>
